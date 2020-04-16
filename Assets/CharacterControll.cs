058a154ac1d6dd569dbdab3b8cc3a3f5351bf2b7
Aleksey Davidenko <24606305+LoS-Kotyara@users.noreply.github.com>
2020-04-16 14:38:27 +0400

    First commit

---
+++ Assets/CharacterControll.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControll : MonoBehaviour
+{
+  public float speed = 2.0f; // скорость перемещения персонажа
+  public float gravity = -9.8f;
+
+  private CharacterController _charController; // ссылка на компонент CharacterController
+
+  void Start()
+  {
+    _charController = GetComponent<CharacterController>(); // доступ к другим компонентам, присоединённых к этому же объекту
+  }
+
+
+  // Update is called once per frame
+  void Update()
+  {
+    // "Horizontal" и "Vertical" - A/D и W/S клавиши на клавиатуре соответственно
+    float deltaX = Input.GetAxis("Horizontal") * speed;
+    float deltaZ = Input.GetAxis("Vertical") * speed;
+
+    Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+    movement.y = gravity; // заставляем персонажа ходить по земле, а не летать над ней
+    
+    movement = Vector3.ClampMagnitude(movement, speed); // ограничение движения по диагонали той же скорости, с которой объект перемещается параллельно осям
+
+    movement *= Time.deltaTime; // делаем так, чтобы значение перемещения по координатам не зависело от частоты кадров
+
+    movement = transform.TransformDirection(movement); // преобразуем вектор движения от локальных к глобальным координатам
+
+    _charController.Move(movement); // вектор перемещает этот компонент CharacterController
+  }
+}
---
+++ Assets/MouseLook.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+  // Структура, сопоставляющая имена параметрам в графическом редакторе Unity
+  public enum RotationAxes
+  {
+    MouseXAndY = 0,
+    MouseX = 1,
+    MouseY = 2
+  }
+
+  // Объявляем объект структуры RotationAxes
+  public RotationAxes axes; 
+
+  // Скорости вращения по осям
+  public float sensitivityHor = 9.0f;
+  public float sensitivityVert = 9.0f;
+
+  // Ограничения вертикального поворота камеры
+  public float minimumVert = -45.0f;
+  public float maximumVert = 45.0f;
+
+  // Угол поворота по вертикали
+  private float _rotationX = 0;
+
+  void Start()
+  {
+    // Отключение влияния среды на поворот персонажа
+    Rigidbody body = GetComponent<Rigidbody>();
+    if (body != null) // проверяем, существует ли этот компонент
+      body.freezeRotation = true;
+  }
+  void Update()
+  {
+    if (axes == RotationAxes.MouseX)
+    {
+      transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0); // метод GetAxis() получает данные, вводимые с помощью мыши. Изменение положения курсора мыши на экране. предел -1 .. 1
+    }
+    else if (axes == RotationAxes.MouseY)
+    {
+      _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert; // увеличиваем угол поворота по вертикали в соответствии с перемещением курсора мыши
+
+      _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert); // фиксируем угол поворота в диапазоне, заданном минимальным и максимальным значениями
+
+      float rotationY = transform.localEulerAngles.y; // сохраняем одинаковый угол вращения вокруг оси Y
+
+      transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0); // создаём новый вектор из сохранённых значений поворота
+    }
+    else
+    {
+      _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+      _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+      float delta = Input.GetAxis("Mouse X") * sensitivityHor; // величина изменения угла поворота
+
+      float rotationY = transform.localEulerAngles.y + delta; // приращение угла поворота через значение delta
+
+      transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+    }
+  }
+}
